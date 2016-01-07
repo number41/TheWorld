0ab20bb71a0ef1ef9e26fdcbf14985d930ca3d41
Sean Peterson <sean.peterson@gmail.com>
2016-01-06 22:19:51 -0600

    [api] Implement HTTP POST action
    Handling HTTP post is done with adding the [HttpPost] attribute to a
    method. Converting the posted body into a model is done with the
    [FromBody] attribute on the input parameter.
    
    Instead of duplicating 'api/trips' all over the place, the entire
    controller is decorated with the [Route] attribute.

--- src/TheWorld/Controllers/Api/TripController.cs
+++ src/TheWorld/Controllers/Api/TripController.cs
@@ -3,10 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheWorld.Models;
 using TheWorld.Models.Repos;
 
 namespace TheWorld.Controllers.Api
 {
+    [Route("api/trips")]
     public class TripController : Controller
     {
         private IWorldRepository repo;
@@ -15,10 +17,17 @@ namespace TheWorld.Controllers.Api
         {
             this.repo = repo;
         }
-        [HttpGet("api/trips")]
+
+        [HttpGet]
         public JsonResult Get()
         {
             return Json(repo.GetAllTripsWithStops());
+        }
+
+        [HttpPost]
+        public JsonResult Post([FromBody] Trip newTrip)
+        {
+            return Json(true);
         }
     }
 }
