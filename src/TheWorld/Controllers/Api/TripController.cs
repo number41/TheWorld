d6f7fdb0ce6ee17fbc304c62428436b3f94318ec
Sean Peterson <sean.peterson@gmail.com>
2016-01-06 22:03:55 -0600

    [api] Initial Trip REST-ish controller
    Again, a lot like Spring MVC so far - especially with TripController and
    AppController having the same structure. The only difference between
    getting HTML back vs. raw JSON is the return type (IActionResult vs.
    JsonResult).
    
    The funky CamelCase resolver thingy in Startup.cs is so that the
    serialized JSON has the typical camelCase structure. Otherwise the JSON
    retains .NET's Casing.
    
    The application is also no longer launching the browser on startup. This
    is to make it easier to test with other tools, such as Postman

---
+++ src/TheWorld/Controllers/Api/TripController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheWorld.Models.Repos;
+
+namespace TheWorld.Controllers.Api
+{
+    public class TripController : Controller
+    {
+        private IWorldRepository repo;
+
+        public TripController(IWorldRepository repo)
+        {
+            this.repo = repo;
+        }
+        [HttpGet("api/trips")]
+        public JsonResult Get()
+        {
+            return Json(repo.GetAllTripsWithStops());
+        }
+    }
+}
